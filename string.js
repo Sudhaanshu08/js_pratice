75674b1f2c61664ead36b4ffbd8b14b98b305561
Sudhanshu <sharmaasudhanshu08@gmail.com>
2023-10-07 19:10:17 +0530

    lets check

---
+++ string.js
@@ -0,0 +1,7 @@
+const name = "Sudhanshu";
+const repoCount = 50;
+
+// console.log(name + repoCount + "Value");   Note :- This is Not a good method to write it.
+
+// Correct Way :-
+console.log(`Hello ji my name is ${name} and my repo Count is ${repoCount}` );
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("sudhanshu")    
+const accountId = 14423
+var accountPass = 12345
+// let accountGmail = sudhanshu56@Gmail.com
+
+console.table([accountId, accountPass])
